4f3475af3afdfe217a28d238aadffffbdfe105e6
Sasha Boguslavska <sbogudlavska@gmail.com>
2023-02-28 17:09:23 +0100

    new

---
+++ project2/script.js
@@ -0,0 +1,9 @@
+for (let row = 0; row<18; row++){
+    const tr = document.createElement('tr') 
+    for (let col = 0; col<18; col++){
+        const td = document.createElement('td')
+        tr.append(td)  
+    }
+    board.append(tr) 
+}
+
